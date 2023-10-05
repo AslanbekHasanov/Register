5ae98327dc1de8186a6c265f24fc9ea6fe742c06
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-10-05 19:29:32 +0500

    Added NpgSql , ConnectionString

--- Register.Backend/DataLayer/RegistrDbContext.cs
+++ Register.Backend/DataLayer/RegistrDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace Register.Backend.DataLayer
+﻿using Microsoft.EntityFrameworkCore;
+using Register.Backend.Model;
+
+namespace Register.Backend.DataLayer
 {
-    public class RegistrDbContext
+    public class RegistrDbContext: DbContext
     {
+        public RegistrDbContext(DbContextOptions<RegistrDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<User> MyProperty { get; set; }
     }
 }
--- Register.Backend/Program.cs
+++ Register.Backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Register.Backend.DataLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<RegistrDbContext>(option => 
+option.UseNpgsql(builder.Configuration.GetConnectionString("RegistrConnectionString")));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
